d46b52ea3da9cfa16b7fa40849dd75c4c0a22421
loki-one <lokesh.jadhav10@gmail.com>
2017-10-02 14:09:29 +0200

    Complete version 1 requirements using add, splice

--- js/client.js
+++ js/client.js
@@ -1,1 +1,15 @@
-console.log("To do list");
+var todos = ['item 1', 'item 2', 'item 3'];
+
+console.log('My todos:',todos);
+
+todos.push('item 4');
+
+console.log('My todos:',todos);
+
+todos[0] = 'item 1 updated';
+
+console.log('My todos:',todos);
+
+todos.splice(3,1);
+
+console.log('My todos:',todos);
