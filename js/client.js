744380a781fd6b4f811d57238c281b98e6c7bbb4
Lokesh Jadhav <lokesh.jadhav10@gmail.com>
2017-10-03 16:29:52 +0200

    Complete Version 5 Requirements

--- js/client.js
+++ js/client.js
@@ -2,7 +2,18 @@ var todoList = {
 
     todos : [],
     displayTodos : function(){
-        console.log('My todos:',this.todos);
+      if(this.todos.length === 0){
+        console.log('Your todo list is empty');
+      }else{
+        console.log('My todos:');
+        for(var i=0; i<this.todos.length;i++){
+            if(this.todos[i].completed){
+              console.log('(x)',this.todos[i].todoText);
+            }else{
+              console.log('( )',this.todos[i].todoText);
+            }
+          }
+      }
     },
     addTodo : function(todoText){
       this.todos.push({
