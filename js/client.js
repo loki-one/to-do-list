7e2baccc0ce5f55f82aeed21e2faad2d9a7aa1b5
loki-one <lokesh.jadhav10@gmail.com>
2017-10-06 16:17:55 +0200

    Refactoring: Using handlers instead of Event Listeners for display todos and toggle all

--- js/client.js
+++ js/client.js
@@ -59,13 +59,12 @@ var todoList = {
     }
 };
 
-var displayTodosButton = document.getElementById('displayTodosButton');
-var toggleAllButton = document.getElementById('toggleAllButton');
-
-displayTodosButton.addEventListener('click', function(){
-    todoList.displayTodos();
-});
-
-toggleAllButton.addEventListener('click', function(){
-    todoList.toggleAll();
-});
+//All methods that handles different events are under handlers
+var handlers = {
+    displayTodos: function(){
+        todoList.displayTodos();
+    },
+    toggleAll: function(){
+        todoList.toggleAll();
+    }
+}
