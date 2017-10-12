28399035aad869231cf10001049c4223c18cf0b0
loki-one <lokesh.jadhav10@gmail.com>
2017-10-12 10:39:44 +0200

    Complete Version 10 Requirements

--- js/client.js
+++ js/client.js
@@ -55,10 +55,8 @@ var handlers = {
       changeTodoTextInput.value = '';
       view.displayTodos();
     },
-    deleteTodo: function(){
-      var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-      todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-      deleteTodoPositionInput.value = '';
+    deleteTodo: function(position){
+      todoList.deleteTodo(position);
       view.displayTodos();
     },
     toggleCompleted: function(){
@@ -98,5 +96,17 @@ var view = {
     deleteButton.textContent = 'Delete';
     deleteButton.className = 'deleteButton';
     return deleteButton;
+  },
+  setUpEventListeners: function(){
+    var todosUl = document.querySelector('ul');
+    todosUl.addEventListener('click', function(event){
+      var elementClicked = event.target;
+      if(elementClicked.className === 'deleteButton'){
+        handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+      }
+    });
   }
+
 };
+
+view.setUpEventListeners();
